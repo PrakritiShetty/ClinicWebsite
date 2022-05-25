5f61d3a9d573c9071311a91bb178b98aa72b6fec
PrakritiShetty <prakritishetty02@gmail.com>
2022-05-25 16:04:00 +0530

    WorkingOnFrontEnd #2

--- client/src/components/index.component.js
+++ client/src/components/index.component.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, {Component}   from 'react';
+import Carousel from 'react-bootstrap/Carousel'  
 import {Link} from 'react-router-dom';
 
 
@@ -6,9 +7,9 @@ export default class Index extends Component
 {
     render()
     {
-        return(
+    return(
 
-        <div style={{margin:10, fontFamily:"Georgia"}}>
+    <div style={{margin:10, fontFamily:"Georgia"}}>
         <header>
         <nav className="navbar  bg-secondary navbar-expand-sm container-fluid">      
         <ul className="navbar-nav ms-auto" >          
@@ -19,10 +20,11 @@ export default class Index extends Component
         </ul>        
         </nav>
         
-        <nav className="navbar-brand" style={{fontFamily:"Georgia"}}>
+        <nav className="navbar-brand" style={{fontFamily:"Times New Roman"}}>
         <ul className="navbar-nav">
         <li className="navbar-item" >
-        <p className="nav-brand"><span class="mb-0 h1">Dr Shettys' Total Dental Care</span></p>
+        <p className="navbar-item" style={{fontFamily:"Lucida Handwriting"}}>Dr Shetty's</p>
+        <p className="nav-brand" style={{textAlign:"center", fontFamily:"Lucida Handwriting"}}><span class="mb-0 h1">TOTAL DENTAL CARE</span></p>
         </li>
         <li className="navbar-item ms-auto" >
         <p class="form-check form-switch ms-auto">
@@ -55,20 +57,51 @@ export default class Index extends Component
             <source src="https://youtu.be/36MEJbSidMg" type="video"></source>
             Your browser does not support the video tag.
         </video> */}
-        <iframe width="100%" height="315"
+        {/* <iframe width="100%" height="315"
         src="https://youtube.com/embed/36MEJbSidMg?controls=0&autoplay=true&muted=1">
-        </iframe>
+        </iframe> */}
 
+        <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="./images/cat2.jpg"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="./images/cat3.jpg"
+      alt="Second slide"
+    />
 
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="./images/cat1.jpg"
+      alt="Third slide"
+    />
 
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
 
+              
         </div>
-
-
-           
-        
-
-        );
+    );
     }
     
 }
