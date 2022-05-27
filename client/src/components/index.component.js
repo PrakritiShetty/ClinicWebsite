0197ab8f30e057df1a92a66084d02eceee5101ac
PrakritiShetty <prakritishetty02@gmail.com>
2022-05-27 15:55:23 +0530

    Frontend almost complete- about component left

--- client/src/components/index.component.js
+++ client/src/components/index.component.js
@@ -1,5 +1,7 @@
 import React, {Component}   from 'react';
-import Carousel from 'react-bootstrap/Carousel'  
+import Carousel from 'react-bootstrap/Carousel'
+import Card from 'react-bootstrap/Card'
+import {AiOutlineHome, AiOutlineFacebook, AiOutlineTwitter, AiOutlineLinkedin} from "react-icons/ai"  
 import {Link} from 'react-router-dom';
 
 
@@ -9,123 +11,253 @@ export default class Index extends Component
     {
     return(
 
-    <div style={{margin:10, fontFamily:"Georgia"}}>
-        <header>
-        <nav className="navbar  bg-secondary navbar-expand-sm container-fluid">      
-        <ul className="navbar-nav ms-auto" >          
-            <li className="navbar-item px-1"><img src="./images/facebookLogo.png" height="25" width="25" ></img></li>
-            <li className="navbar-item "><img src="./images/instagramLogopng.png" height="25" width="25" ></img></li>
-            <li className="navbar-item px-1"><img src="./images/linkedinLogopng.png" height="25" width="25" ></img></li>
-            <li className="navbar-item "><p style={{fontSize:10, margin:10, color:"white"}}>Copyright@2022</p></li>  
-        </ul>        
-        </nav>
-        
-        <nav className="navbar-brand" style={{fontFamily:"Times New Roman", width:'100%'}}>
-        <ul className="navbar-nav" style={{backgroundImage: 
-        "url('./images/background1.webp')",
-        width:'100%',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'space'}}>
-        <li className="navbar-item" >
-        
-        <p className="navbar-item" style={{fontFamily:"Lucida Handwriting"}}
-        >Dr Shetty's</p>
-        <p className="nav-brand" style={{textAlign:"center", fontFamily:"Lucida Handwriting"}}><span class="mb-0 h1">TOTAL DENTAL CARE</span></p>
-        </li>
-        <li className="navbar-item ms-auto" >
-        <p class="form-check form-switch ms-auto">
-        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-        <label class="form-check-label" for="flexSwitchCheckDefault" style={{fontSize: '10px'}}>Theme</label>
-        </p>
-          </li>         
-        </ul>
-       
-        </nav>
-
-        </header>
-        <nav className="navbar navbar-dark bg-light navbar-expand-lg">
-        <div className="collapse navbar-collapse">
-        <ul className="navbar-nav me-auto">
-          <li style={{fontFamily:"Lucida Handwriting"}}><em><b>Dr Sandhya Shankar Shetty</b></em> & <em><b>Dr Pratiksha Shankar Shetty</b></em></li>
-        </ul>
-        <ul className="navbar-nav ms-auto">
-          <li className="navbar-item" >
-          <p className="nav-link" style={{color:"grey"}}>About</p>
-          </li>
-          <li className="navbar-item" >
-          <p className="nav-link" style={{color:"grey"}}>Services</p>
-          </li>         
-        </ul>
-        <button type="button" class="btn btn-dark" style={{margin:5}}>
-            Book an Appointment
-        </button>
-        </div>
-        </nav>
-
-        {/* <video width="100%"  autoplay>
-            <source src="https://youtu.be/36MEJbSidMg" type="video"></source>
-            Your browser does not support the video tag.
-        </video> */}
-        {/* <iframe width="100%" height="315"
-        src="https://youtube.com/embed/36MEJbSidMg?controls=0&autoplay=true&muted=1">
-        </iframe> */}
-
-        <Carousel>
-          <Carousel.Item>
-          <img
-            className="d-block w-100 h-50"
-            src="./images/graygeneral.jpg"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h1 style={{color:"black"}}><b>GENERAL</b></h1>
-            <p style={{color:"black",fontFamily:"Lucida Handwriting"}}>Simple procedures for everyday problems</p>
-            <button type="button" class="btn btn-outline-info me-1" >Fillings</button>
-            <button type="button" class="btn btn-outline-info me-1" >Scaling</button>
-            <button type="button" class="btn btn-outline-info me-1" >Crowns</button>
-            <button type="button" class="btn btn-outline-info me-1" >Emergency</button>
+      <div style={{margin:10, fontFamily:"Georgia"}}>
+      <header>
+          <nav className="navbar navbar-expand-sm container-fluid" style={{backgroundColor:"Purple"}}>
+              <ul className="navbar-nav ms-auto">
+                  <li className="navbar-item px-1"><img src="./images/facebookLogo.png" height="25" width="25"></img></li>
+                  <li className="navbar-item "><img src="./images/instagramLogopng.png" height="25" width="25"></img></li>
+                  <li className="navbar-item px-1"><img src="./images/linkedinLogopng.png" height="25" width="25"></img>
+                  </li>
+                  <li className="navbar-item ">
+                      <p style={{fontSize:10, margin:10, color:"white"}}>Copyright@2022</p>
+                  </li>
+              </ul>
+          </nav>
+  
+          <nav className="navbar-brand" style={{fontFamily:"Times New Roman", width:'100%'}}>
+              <ul className="navbar-nav" style={{backgroundImage: 
+      "url('./images/back.png')",
+      width:'100%',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'space'}}>
+                  <li className="navbar-item">
+  
+                      <p className="navbar-item" style={{fontFamily:"Lucida Handwriting"}}>Dr Shetty's</p>
+                      </li>
+                      <li className="navbar-item px-5">
+                        <nav className="navbar navbar-expand-lg container-fluid">
+                        <ul className="navbar-nav">
+                      
+                      <li><img src='./images/logotooth.png' height="100" width="200"></img></li>
+                      
+                      <li><p className="nav-brand" style={{textAlign:"center", fontFamily:"Georgia" ,fontSize:75}}>TOTAL DENTAL CARE</p></li>
+                              </ul>
+                              </nav>
+                  </li>
+                  <li className="navbar-item ms-auto">
+                      <p class="form-check form-switch ms-auto">
+                          <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                          <span><label class="form-check-label" for="flexSwitchCheckDefault"
+                              style={{fontSize: '10px'}}>Theme</label></span>
+                      </p>
+                  </li>
+              </ul>
+  
+          </nav>
+  
+      </header>
+      
+      <nav className="navbar navbar-dark navbar-expand-lg" style={{backgroundColor:"AliceBlue"}}>
+          <div className="collapse navbar-collapse">
+              <ul className="navbar-nav me-auto">
+                  <li style={{fontFamily:"Lucida Handwriting"}}><em><b>Dr Sandhya Shankar Shetty</b></em> & <em><b>Dr
+                              Pratiksha Shankar Shetty</b></em></li>
+                  <br></br>
+              </ul>
+  
+  
+              <ul className="navbar-nav ms-auto">
+                  <li className="navbar-item">
+                      <p className="nav-link" style={{color:"grey"}}>About</p>
+                  </li>
+                  <li className="navbar-item">
+                      <p className="nav-link" style={{color:"grey"}}>Services</p>
+                  </li>
+              </ul>
+              <button type="button" class="btn btn-dark" style={{margin:5}}>
+                  Book an Appointment
+              </button>
+  
+          </div>
+      </nav>
+      
+    
+ 
+  
+  
+      {/* <video width="100%" autoplay>
+          <source src="https://youtu.be/36MEJbSidMg" type="video">
+          </source>
+          Your browser does not support the video tag.
+      </video> */}
+      {/* <iframe width="100%" height="315" src="https://youtube.com/embed/36MEJbSidMg?controls=0&autoplay=true&muted=1">
+      </iframe> */}
+  
+  
+      <Carousel>
+          <Carousel.Item style={{height:"500px"}}>
             
-          </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-          <img
-            className="d-block w-100 h-50"
-            src="./images/graycosmetic.jpg"
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h1 style={{color:"black"}}><b>COSMETIC</b></h1>
-            <p style={{color:"black", fontFamily:"Lucida Handwriting"}}>Enhancing the outward appearance of your teeth</p>
-            <button type="button" class="btn btn-outline-info me-1 mb-1" >Whitening</button>
-            <button type="button" class="btn btn-outline-info me-1 mb-1" >Aligners/Braces</button>
-            <button type="button" class="btn btn-outline-info me-1 mb-1" >Bonding</button>
-            <button type="button" class="btn btn-outline-info me-1 mb-1" >Veneers</button>
-            <button type="button" class="btn btn-outline-info me-1" >Smile Design</button>
-            <button type="button" class="btn btn-outline-info me-1" >Full Mouth Rehab</button>
-          </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-          <img
-            className="d-block w-100 h-50"
-            src="./images/graysurgical.jpg"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h1 style={{color:"black"}}><b>SURGICAL</b></h1>
-            <p style={{color:"black", fontFamily:"Lucida Handwriting"}}>Slightly longer procedures for comprehensive care</p>
-            <button type="button" class="btn btn-outline-info me-1 mb-1" >Implants</button>
-            <button type="button" class="btn btn-outline-info me-1 mb-1" >Extractions</button>
-            <button type="button" class="btn btn-outline-info me-1 mb-1" >Root Canals</button>
-            <button type="button" class="btn btn-outline-info me-1 mb-1" >Periodontal Flaps</button>
-          </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-
-
-
+              <img className="d-block w-100 h-100" src="./images/image3.webp" alt="First slide" />
               
-        </div>
+              <Carousel.Caption>
+              <div style={{backgroundColor:"rgba(255,255,255,.5)", height:"max-height", padding:"10px", borderRadius:"50px"}}>
+                  <h1 style={{color:"black"}}><b>GENERAL</b></h1>
+                  <p style={{color:"black",fontFamily:"Lucida Handwriting" , opacity:1}}>Simple procedures for everyday problems</p>
+                  <button type="button" class="btn btn-outline-danger me-1"  ><b>Fillings</b></button>
+                  <button type="button" class="btn btn-outline-danger me-1"><b>Scaling</b></button>
+                  <button type="button" class="btn btn-outline-danger me-1"><b>Crowns</b></button>
+                  <button type="button" class="btn btn-outline-danger me-1"><b>Emergency</b></button>
+                  </div>
+              </Carousel.Caption>
+              
+          </Carousel.Item>
+          <Carousel.Item style={{height:"500px"}}>
+              <img className="d-block w-100 h-100" src="./images/image2.jpg" alt="Second slide" height="100%" />
+              <Carousel.Caption>
+              <div style={{backgroundColor:"rgba(255,255,255,.5)", height:"max-height", padding:"10px", borderRadius:"50px"}}>
+                  <h1 style={{color:"black"}}><b>COSMETIC</b></h1>
+                  <p style={{color:"black", fontFamily:"Lucida Handwriting"}}>Enhancing the outward appearance of your
+                      teeth</p>
+                  <button type="button" class="btn btn-outline-danger me-1 mb-1"><b>Whitening</b></button>
+                  <button type="button" class="btn btn-outline-danger me-1 mb-1"><b>Aligners/Braces</b></button>
+                  <button type="button" class="btn btn-outline-danger me-1 mb-1"><b>Bonding</b></button>
+                  <button type="button" class="btn btn-outline-danger me-1 mb-1"><b>Veneers</b></button>
+                  <button type="button" class="btn btn-outline-danger me-1"><b>Smile Design</b></button>
+                  <button type="button" class="btn btn-outline-danger me-1"><b>Full Mouth Rehab</b></button>
+                  </div>
+              </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item style={{height:"500px"}}>
+              <img className="d-block w-100 h-100" src="./images/image1.webp" alt="Third slide" />
+  
+              <Carousel.Caption>
+              <div style={{backgroundColor:"rgba(255,255,255,.5)", height:"max-height", padding:"10px", borderRadius:"50px"}}>
+                  <h1 style={{color:"black"}}><b>SURGICAL</b></h1>
+                  <p style={{color:"black", fontFamily:"Lucida Handwriting"}}>Slightly longer procedures for comprehensive
+                      care</p>
+                  <button type="button" class="btn btn-outline-danger me-1 mb-1"><b>Implants</b></button>
+                  <button type="button" class="btn btn-outline-danger me-1 mb-1"><b>Extractions</b></button>
+                  <button type="button" class="btn btn-outline-danger me-1 mb-1"><b>Root Canals</b></button>
+                  <button type="button" class="btn btn-outline-danger me-1 mb-1"><b>Periodontal Flaps</b></button>
+                  </div>
+              </Carousel.Caption>
+          </Carousel.Item>
+      </Carousel>
+
+      
+    <br></br>
+    <div style={{height:"500px"}}>
+
+    <nav className="navbar  navbar-expand-sm container-fluid" style={{scrollbarWidth:"none",WebkitScrollbarDisplay:"none", backgroundColor:"LavenderBlush"}}>
+        <ul className="navbar-nav me-auto" style={{ display: "grid",
+gridAutoFlow: "column",
+overflowX: "scroll",
+overscrollBehaviorInline: "contain", }}>
+            <li className="navbar-item px-3" style={{loading:"lazy", height:160}}>
+                <Card style={{ width: '30rem', height:'10rem', backgroundColor:"rgba(100,0,0,.5)" }}>
+
+                    <Card.Body style={{scrollbarWidth:"none"}}>
+                        <Card.Title style={{color:"white"}}>Name</Card.Title>
+                        <Card.Text style={{color:"white"}}>
+                            Sample text
+                        </Card.Text>
+                        {/* <Button variant="primary">Go somewhere</Button> */}
+                    </Card.Body>
+                </Card>
+            </li>
+
+            <li className="navbar-item px-3">
+                <Card style={{ width: '30rem', height:'10rem',backgroundColor:"rgba(100,0,0,.5)" }}>
+
+                    <Card.Body style={{scrollbarWidth:"none"}}>
+                        <Card.Title style={{color:"white"}}>Name</Card.Title>
+                        <Card.Text style={{color:"white"}}>
+                            Sample text
+                        </Card.Text>
+                        {/* <Button variant="primary">Go somewhere</Button> */}
+                    </Card.Body>
+                </Card>
+            </li>
+            <li className="navbar-item px-3" style={{loading:"lazy"}}>
+                <Card style={{ width: '30rem' , height:'10rem', backgroundColor:"rgba(100,0,0,.5)"}}>
+
+                    <Card.Body style={{scrollbarWidth:"none"}}>
+                        <Card.Title style={{color:"white"}}>Name</Card.Title>
+                        <Card.Text style={{color:"white"}}>
+                            Sample text
+                        </Card.Text>
+                        {/* <Button variant="primary">Go somewhere</Button> */}
+                    </Card.Body>
+                </Card>
+            </li>
+            <li className="navbar-item px-3" style={{loading:"lazy"}}>
+                <Card style={{ width: '30rem' , height:'10rem', backgroundColor:"rgba(100,0,0,.5)" }}>
+
+                    <Card.Body style={{scrollbarWidth:"none"}}>
+                        <Card.Title style={{color:"white"}}>Name</Card.Title>
+                        <Card.Text style={{color:"white"}}>
+                            Sample text
+                        </Card.Text>
+                        {/* <Button variant="primary">Go somewhere</Button> */}
+                    </Card.Body>
+                </Card>
+            </li>
+            <li className="navbar-item px-3" style={{loading:"lazy"}}>
+                <Card style={{ width: '30rem', height:'10rem', backgroundColor:"rgba(100,0,0,.5)" }}>
+
+                    <Card.Body style={{scrollbarWidth:"none"}}>
+                        <Card.Title style={{color:"white"}}>Name</Card.Title>
+                        <Card.Text style={{color:"white"}}>
+                            Sample text
+                        </Card.Text>
+                        {/* <Button variant="primary">Go somewhere</Button> */}
+                    </Card.Body>
+                </Card>
+            </li>
+        </ul>
+    </nav>
+
+    <nav className="navbar  bg-light navbar-expand-sm container-fluid">
+        <ul className="navbar-nav">
+         
+            <li className="navbar-item px-6"><b><br></br><br></br><br></br>REACH US:</b> </li>
+            <li className="navbar-item px-5"><br></br>Dr Shetty's Total Dental Care<br></br>Shop #9, Saidham Complex<br></br>P.K road
+                Mumbai- 400076<br></br>Maharashtra, India <br></br> <br></br>+(91) 98XXXXXXXX</li>
+
+<li>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3768.63998435371!2d72.94209841393148!3d19.167230054081532!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b8f7c2d76931%3A0xa2d85150fddd96c8!2sDr%20Sandhya&#39;s%20Total%20Dental%20Care!5e0!3m2!1sen!2sin!4v1653633865846!5m2!1sen!2sin" width="800" height="200" style={{border:0}} referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </li>
+        </ul>
+    </nav>
+
+        <nav className="navbar  navbar-expand-sm container-fluid"
+            style={{height:"max-height",backgroundColor:"Purple"}}>
+            <ul className="navbar-nav me-auto" style={{color:"white"}}>
+                <li className="navbar-item px-5"><br></br>
+                    <h3>Position yourself with precision</h3>
+                    <br></br><small>Call us at: +(91)98XXXXXXX</small> <br></br> <small>Email us at:
+                        danger@example.com</small>
+                </li>
+                <li className="navbar-item px-5"><br></br>QUICK LINKS:<br></br><br></br>Home<br></br>About
+                    Us<br></br>Technology<br></br>Careers<br></br>Documentation<br></br>Contact Us</li>
+                <li className="navbar-item px-5"><br></br>ADDRESS:<br></br><br></br>Dr Shetty's Total Dental Care<br></br>Shop #9, Saidham Complex<br></br>P.K road
+                Mumbai- 400076<br></br>Maharashtra, India
+                    <br></br> <br></br>+(91) 98XXXXXXXX</li>
+                <li className="navbar-item px-5"><br></br>SOCIAL:<br></br><br></br>
+                    <AiOutlineFacebook size={30} />
+                    <AiOutlineTwitter size={30} />
+                    <AiOutlineLinkedin size={30} />
+                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>Copyright@2022</li>
+            </ul>
+        </nav>
+    </div>
+
+  
+  
+  
+  
+  </div>
     );
     }
     
